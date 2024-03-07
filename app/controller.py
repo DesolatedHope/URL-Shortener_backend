bfa067c1e842df538cecd6825edc8fa15d9d2dd6
Himdyuti1 <himdyutisandilya@kgpian.iitkgp.ac.in>
2024-03-07 19:59:51 +0530

    Fixed delete error

--- app/controller.py
+++ app/controller.py
@@ -132,6 +132,7 @@ def inactivateURL():
 def deleteURL():
     data=request.get_json()
     shortURL=data['shortURL']
+    shortURL=shortURL[-7:]
     email=get_jwt_identity()
     result=db.websites.find_one({"shortURL":shortURL})
     db.variables.update_one({"_id":"counter"},{"$inc":{"websites":-1}})
